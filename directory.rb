887dc62c45cfe79f1d8c508671f9a804651cf551
umut <umut_baykan@hotmail.com>
2023-03-06 12:03:06 +0000

    rewrote the student list to be based on user input

--- directory.rb
+++ directory.rb
@@ -1,17 +1,17 @@
 # Arraying the students
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+# students = [
+#   {name: "Dr. Hannibal Lecter", cohort: :november},
+#   {name: "Darth Vader", cohort: :november},
+#   {name: "Nurse Ratched", cohort: :november},
+#   {name: "Michael Corleone", cohort: :november},
+#   {name: "Alex DeLarge", cohort: :november},
+#   {name: "The Wicked Witch of the West", cohort: :november},
+#   {name: "Terminator", cohort: :november},
+#   {name: "Freddy Krueger", cohort: :november},
+#   {name: "The Joker", cohort: :november},
+#   {name: "Joffrey Baratheon", cohort: :november},
+#   {name: "Norman Bates", cohort: :november}
+# ]
 
 def print_header
   puts "The students of Villains Academy"
@@ -26,6 +26,25 @@ def print_footer(names)
   puts "Overall, we have #{names.length} great students"
 end
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  ## empty array where students info will be pushed to
+  students = []
+  # get the name
+  name = gets.chomp
+  while name != "" do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    ## gets another name, until the loop ends
+    name = gets.chomp
+  end
+  ## returns the array of students
+  students
+end
+
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
